ac2ecf485d54adb00bd584464997e8519f850e59
tubi55 <tubi55@nate.com>
2022-01-11 02:19:10 +0900

    모션중 재이벤트 방지

--- js/loop_slider.mjs
+++ js/loop_slider.mjs
@@ -4,33 +4,53 @@ const slider = document.querySelector('#slider');
 const ul = slider.querySelector('ul');
 const prev = slider.querySelector('.prev');
 const next = slider.querySelector('.next');
+const speed = 2000;
+let enableClick = true;
 
 ul.style.left = '-100%';
+ul.prepend(ul.lastElementChild);
 
 next.addEventListener('click', e=>{
     e.preventDefault();
 
-    new Animate(ul,{
-        prop: 'left',
-        value: '-200%',
-        duration: 1000,
-        callback: ()=>{
-            ul.style.left = '-100%';
-            ul.append(ul.firstElementChild);
-        }
-    })
+    if(enableClick){
+        enableClick = false;
+        nextSlide();
+    }    
 })
 
 prev.addEventListener('click', e=>{
     e.preventDefault();
 
+    if(enableClick){
+        enableClick = false;
+        prevSlide();
+    }
+    
+})
+
+function nextSlide(){
+    new Animate(ul,{
+        prop: 'left',
+        value: '-200%',
+        duration: speed,
+        callback: ()=>{
+            ul.style.left = '-100%';
+            ul.append(ul.firstElementChild);
+            enableClick = true;
+        }
+    })
+}
+
+function prevSlide(){
     new Animate(ul, {
         prop: 'left',
         value: '0%',
-        duration: 1000,
+        duration: speed,
         callback: ()=>{
             ul.style.left = '-100%',
             ul.prepend(ul.lastElementChild);
+            enableClick = true;
         }
     })
-})
+}
